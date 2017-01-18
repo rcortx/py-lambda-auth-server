72813aea4e6363e5c81c3c10df68061d2b447c8c
Raunak Bhandari <ronniebhase@gmail.com>
2017-01-18 15:30:49 +0530

    Update lambda_function.py

--- lambda_function.py
+++ lambda_function.py
@@ -17,7 +17,8 @@ def lambda_handler(event, context=None):
                 response["message"] = e.message
                 response["errors"] = e.errors
                 # removing request_dump data
-                del response["errors"]["request_dump"]
+                if "request_dump" in response["errors"]:
+                        del response["errors"]["request_dump"]
                 for _k, _v in response["errors"].items():
                     response["errors"][_k] = str(_v)
             return response
